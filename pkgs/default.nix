7368ee33972b818c64f579df3217618acbff6cd7
Nick Spinale <nick@nickspinale.com>
2024-06-26 16:32:19 +0000

    Remove platforms

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -15,26 +15,5 @@ self: with self;
 
   dtbHelpers = callPackage ./dtb-helpers {};
 
-  qemu-base = callPackage ./platforms/qemu/qemu { targets = []; };
-
-  qemu-arm = qemu-base.override { targets = [ "arm-softmmu" ]; };
-  qemu-aarch64 = qemu-base.override { targets = [ "aarch64-softmmu" ]; };
-  qemu-x86_64 = qemu-base.override { targets = [ "x86_64-softmmu" ]; };
-
-  qemu-aarch64-user = qemu-base.override { targets = [ "aarch64-linux-user" ]; };
-
-  qemu-all = qemu-base.override {
-    targets = [
-      "arm-softmmu" "aarch64-softmmu" "x86_64-softmmu"
-    ];
-  };
-
-  soc-term = callPackage ./platforms/qemu/soc-term {};
-
-
-  raspios = callPackage ./platforms/raspberry-pi/raspios {};
-  raspberry-pi-firmware = callPackage ./platforms/raspberry-pi/firmware {};
-
-
   eval = callPackage ./eval.nix {};
 }
