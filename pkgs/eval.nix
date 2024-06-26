b1973724ed5e63a7e98999b4130b17953f7adbc1
Nick Spinale <nick@nickspinale.com>
2024-06-26 16:55:10 +0000

    Flatten attribute structure

--- pkgs/eval.nix
+++ pkgs/eval.nix
@@ -1,22 +1,18 @@
 { lib, pkgs }:
 
-{
+{ modules }:
 
-  eval = { modules }:
+let
+  metaModule = {
+    config = {
+      _module.args.pkgs = lib.mkForce pkgs;
+      _module.check = true;
+    };
+  };
 
-    let
-      metaModule = {
-        config = {
-          _module.args.pkgs = lib.mkForce pkgs;
-          _module.check = true;
-        };
-      };
+  baseModule = import ../modules;
 
-      baseModule = import ../modules;
-
-    in
-      lib.evalModules {
-        modules = [ metaModule baseModule ] ++ modules;
-      };
-
+in
+lib.evalModules {
+  modules = [ metaModule baseModule ] ++ modules;
 }
